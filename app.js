004b7d147af849da485ee86c8f58369e51d1a4fe
Flame <yaolynzc@126.com>
2016-09-26 00:17:29 +0800

    完成demo

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var redis = require('./models/redis.js');
 var bodyParser = require('body-parser');
+var mongodb = require('./models/mongodb.js');
 
 var app = express();
 // app.use(express.bodyParser());
@@ -34,13 +35,71 @@ app.get('/',function(req,res){
   if(!req.query.user){
     return res.json({code:0,msg:"信息不对称"});
   }
-  if(req.query.type && (["male","female"].indexOf(req.query.type) === -1)){
+  if(req.query.type && (["male","female","all"].indexOf(req.query.type) === -1)){
     return res.json({
       code:0,
       msg:"类型错误"
     });
   }
   redis.pick(req.query,function(result){
+    if(result.code === 1){
+      mongodb.save(req.query.user,result.msg,function(err){
+        if(err){
+          return res.json({
+            code:0,
+            msg:"获取漂流瓶失败，请重试！"
+          });
+        }
+        // return res.json(result); 
+        //原书中此行代码会导致"Can't set headers after they are sent."报错，注释此行即可解决
+      });
+    }
+    res.json(result);
+  });
+});
+
+// 扔回海里一个漂流瓶
+// POST owner=xxx&type=xxx&content=xxx&time=xxx
+app.post('/back',function(req,res){
+  redis.throwBack(req.body,function(result){
+    res.json(result);
+  });
+});
+
+// 获取一个用户所有的漂流瓶
+// GET /user/nswbmw
+app.get('/user/:user',function(req,res){
+  mongodb.getAll(req.params.user,function(result){
+    res.json(result);
+  });
+});
+
+// 获取特定id的漂流瓶
+// GET /bottle/529a8
+app.get('/bottle/:_id',function(req,res){
+  mongodb.getOne(req.params._id,function(result){
+    res.json(result);
+  });
+});
+
+// 回复特定id的漂流瓶
+// POST user=xxx&content=xxx[&time=xxx]
+app.post('/reply/:_id',function(req,res){
+  if(!(req.body.user && req.body.content)){
+    return callback({
+      code:0,
+      msg:"回复信息不完整！"
+    });
+  }
+  mongodb.reply(req.params._id,req.body,function(result){
+    res.json(result);
+  });
+});
+
+// 删除特定id的漂流瓶
+// GET /delete/529a8b
+app.get('/delete/:_id',function(req,res){
+  mongodb.delete(req.params._id,function(result){
     res.json(result);
   });
 });
--- models/redis.js
+++ models/redis.js
@@ -1,72 +1,143 @@
 var redis = require('redis'),
     // client = redis.createClient();
-    client = redis.createClient(6379,'220.249.123.36');
-  
+    client = redis.createClient(6379,'220.249.123.36'),
+    client2 = redis.createClient(6379,'220.249.123.36'),
+    client3 = redis.createClient(6379,'220.249.123.36');
+    
 // 扔一个漂流瓶
 exports.throw = function(bottle,callback){
-  bottle.time = bottle.time || Date.now();
-  // 为每个漂流瓶随机生成一个id
-  var bottleId = Math.random().toString(16);
-  var type = {
-    male:0,
-    female:1
-  }
-  // 根据漂流瓶类型的不同将漂流瓶保存到不同的数据库
-  client.select(type[bottle.type],function(){
-    console.log(type[bottle.type]);
-    //以hash类型保存漂流瓶对象
-    client.HMSET(bottleId,bottle,function(err,result){
-      if(err){
-        return callback({code:0,msg:"过会儿再试试吧！"});
+  // 先到2号数据库检查用户是否超过扔瓶次数限制
+  client2.select(2,function(){
+    // 获取该用户扔瓶次数
+    client2.get(bottle.owner,function(err,result){
+      if(result >= 10){
+        return callback({
+          code:0,
+          msg:"今天扔瓶子的机会已经用完啦~"
+        });
       }
-      // 返回结果，成功时返回OK
-      callback({
-        code:1,
-        msg:result
+      // 扔瓶次数加1
+      client2.incr(bottle.owner,function(){
+        // 检查是否是当天第一次扔瓶子
+        // 若是，则设置记录该用户扔瓶次数键的生存期为1天
+        // 若不是，生存期保持不变
+        client2.ttl(bottle.owner,function(err,ttl){
+          if(ttl === -1){
+            client2.expire(bottle.owner,86400);
+          }
+        });
       });
-      client.expire(bottleId,86400);
+
+      // 保留不变的代码块
+      bottle.time = bottle.time || Date.now();
+      // 为每个漂流瓶随机生成一个id
+      var bottleId = Math.random().toString(16);
+      var type = {
+        male:0,
+        female:1
+      }
+      // 根据漂流瓶类型的不同将漂流瓶保存到不同的数据库
+      client.select(type[bottle.type],function(){
+        console.log(type[bottle.type]);
+        //以hash类型保存漂流瓶对象
+        client.HMSET(bottleId,bottle,function(err,result){
+          if(err){
+            return callback({code:0,msg:"过会儿再试试吧！"});
+          }
+          // 返回结果，成功时返回OK
+          callback({
+            code:1,
+            msg:result
+          });
+          client.expire(bottleId,86400);
+        });
+      });
     });
   });
 }
 
 // 捡一个漂流瓶
 exports.pick = function(info,callback){
-  // 20%概率捡到海星
-  if(Math.random() <= 0.2){
-    return callback({
-      code:0,
-      msg:"海星"
-    });
-  }
-  var type = {all:Math.round(Math.random()),male:0,female:1};
-  info.type = info.type || 'all';
+  // 先到3号数据库检查用户是否超过捡瓶次数限制
+  client3.select(3,function(){
+    // 获取该用户捡瓶次数
+    client3.get(info.user,function(err,result){
+      if(result >= 10){
+        return callback({
+          code:0,
+          msg:'今天捡到瓶子的机会已经用完了'
+        });
+      }
+      //捡瓶次数加1
+      client3.incr(info.user,function(){
+        // 检查是否是当天第一次捡瓶子
+        // 若是，则设置记录该用户捡瓶次数键的生存期为1天
+        // 如不是，生存期保持不变
+        client3.ttl(info.user,function(err,ttl){
+          if(ttl === 1){
+            client3.expire(info.user,86400);
+          }
+        });
+      });
 
-  // 根据请求的瓶子类型到不同的数据库中取
-  client.select(type[info.type],function(){
-    // 随机返回一个漂流瓶 id
-    client.randomkey(function(err,bottleId){
-      if(!bottleId){
+      // 20%概率捡到海星
+      if(Math.random() <= 0.2){
         return callback({
           code:0,
           msg:"海星"
         });
       }
-      // 根据漂流瓶id取到漂流瓶完整信息
-      client.hgetall(bottleId,function(err,bottle){
-        if(err){
-          return callback({
-            code:0,
-            msg:"漂流瓶破损了..."
+      var type = {all:Math.round(Math.random()),male:0,female:1};
+      info.type = info.type || 'all';
+
+      // 根据请求的瓶子类型到不同的数据库中取
+      client.select(type[info.type],function(){
+        // 随机返回一个漂流瓶 id
+        client.randomkey(function(err,bottleId){
+          if(!bottleId){
+            return callback({
+              code:0,
+              msg:"海星"
+            });
+          }
+          // 根据漂流瓶id取到漂流瓶完整信息
+          client.hgetall(bottleId,function(err,bottle){
+            if(err){
+              return callback({
+                code:0,
+                msg:"漂流瓶破损了..."
+              });
+            }
+            // 返回结果，成功时包含捡到的漂流瓶信息
+            callback({
+              code:1,
+              msg:bottle
+            });
+            // 从Redis中删除该漂流瓶
+            client.del(bottleId);
           });
-        }
-        // 返回结果，成功时包含捡到的漂流瓶信息
-        callback({
-          code:1,
-          msg:bottle
         });
-        // 从Redis中删除该漂流瓶
-        client.del(bottleId);
       });
+    });
+  });
+}
+
+// 将捡到的漂流瓶扔回海里
+exports.throwBack = function(bottle,callback){
+  var type = {male:0,female:1};
+  //为漂流瓶随机生成一个id
+  var bottleId = Math.random().toString(16);
+  //根据漂流瓶类型的不同将漂流瓶保存到不同的数据库
+  client.select(type[bottle.type],function(){
+    // 以hash类型保存漂流瓶对象
+    client.hmset(bottleId,bottle,function(err,result){
+      if(err){
+        return callback({code:0,msg:'过会儿再试试吧！'});
+      }
+      // 返回结果，成功时返回OK
+      callback({code:1,msg:result});
+      //根据漂流瓶的原始时间戳设置生存期
+      client.pexpire(bottleId,bottle.time + 86400000 - Data.now());
     });
   });
 }
